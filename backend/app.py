75c6791a176ec606486dfbf4cadd7ce968e91d83
UnimarksLegal <dsa@unimarkslegal.com>
2025-11-03 10:23:31 +0530

    2days login window

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ def login():
 
     valid_users = {
         os.getenv('ADMIN_USER'): os.getenv('ADMIN_PASS'),
-        os.getenv('SALES_USER'): os.getenv('SALES_PASS'),os.getenv('MD_USER'): os.getenv('MD_PASS')
+        os.getenv('TEST_USER'): os.getenv('TEST_PASS'),os.getenv('MD_USER'): os.getenv('MD_PASS')
     }
 
     if username in valid_users and password == valid_users[username]:
@@ -153,4 +153,3 @@ def update_department(dept_name):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
